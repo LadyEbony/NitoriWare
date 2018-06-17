30f38eacc19cff175e76c5c18cc2380ada36a253
LadyEbony <jgarc129@ucr.edu>
2018-06-16 17:51:03 -0700

    Added KosuzuReadController. Determines amont of input and reads inputs.

---
+++ Assets/Microgames/KosuzuRead/Scripts/KosuzuReadController.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace NitorInc.KosuzuRead {
+    public class KosuzuReadController : MonoBehaviour {
+
+        [Header("Button Press Requirements")]
+        [SerializeField] private int PagesNeeded;
+        [SerializeField] private int PagesRead = 0;
+
+        [Header("Difficulty parameters")]
+        [SerializeField] private float ButtonPressesPerSecond = 2.0f;
+        [SerializeField] private float ReactionTime = 0.4f;
+
+        void Start() {
+            // Get Time remaining for game, subtract standard reaction time, and multiply by its buttonpressespersecond
+            PagesNeeded = Mathf.FloorToInt((MicrogameTimer.instance.beatsLeft * StageController.beatLength - ReactionTime) 
+                * ButtonPressesPerSecond);
+    
+        }
+
+        // Update is called once per frame
+        void Update() {
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                PagesRead++;
+            }   
+        }
+    }
+
+}
